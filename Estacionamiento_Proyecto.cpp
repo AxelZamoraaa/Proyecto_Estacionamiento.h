25002363bce4c0d1eb1a277e1b061aa813a9b847
Axx_zam <saxelzamco@gmail.com>
2026-09-13 19:05:57 -0600

    Update Estacionamiento_Proyecto.cpp

--- Estacionamiento_Proyecto.cpp
+++ Estacionamiento_Proyecto.cpp
@@ -170,17 +170,17 @@ int main() {
                         int indice = numLugar - 1;
                         bool estabaOcupado = false;
 
-                       == "sedan") {
-                            estabaOcupado = lugaressedan[indice];
-                            if (estabaOcupado) {
-                                lugaressedan[indice] = false;
-                                ocupsedan--; if (tipo == "compacto") {
+                        if (tipo == "compacto") {
                             estabaOcupado = lugarecompacto[indice];
                             if (estabaOcupado) {
                                 lugarecompacto[indice] = false;
                                 ocupcompacto--;
                             }
-                        } else if (tipo 
+                        } else if (tipo == "sedan") {
+                            estabaOcupado = lugaressedan[indice];
+                            if (estabaOcupado) {
+                                lugaressedan[indice] = false;
+                                ocupsedan--;
                             }
                         } else if (tipo == "suv") {
                             estabaOcupado = lugaressuv[indice];
